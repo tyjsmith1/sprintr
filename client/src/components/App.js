e5fea3a3396e9dbb027379b53a9ada6e1b6cecd7
tylersmith <tyjsmith1@gmail.com>
2024-02-03 17:10:47 -0600

    more styling on log in

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import LoginForm from "./LoginForm/LoginForm";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="logo">sprintr.</h1>
-      <LoginForm/>
+      <Navbar/>
+      <div className="login-div">
+        <LoginForm/>
+      </div>
     </div>
+
   )
 }
 
