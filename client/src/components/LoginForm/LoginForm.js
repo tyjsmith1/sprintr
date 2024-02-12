a6388bc180b21411bd4abbda07848b520b2163e8
tylersmith <tyjsmith1@gmail.com>
2024-02-12 10:55:17 -0600

    backgrounds

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -6,27 +6,28 @@ import { NavLink } from "react-router-dom";
 function LoginForm(){
 
     return (
-        <div className="wrapper">
-            <form action="">
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUser className="icon"/>
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
-                    <FaLock className="icon"/>
-                </div>
-                <div className="remember-forgot">
-                    <label><input type="checkbox"/>Remember Me</label>
-                    <a href="#">Forgot password?</a>
-                </div>
-                <NavLink to="dashboard"><button className="login-btn" type="submit">Login</button></NavLink>
-                <div className="register-link">
-                    <p>Don't have an account? <NavLink to="/signup">Register</NavLink></p>
-
-                </div>
-            </form>
+        <div>
+            <div className="wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required/>
+                        <FaUser className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required/>
+                        <FaLock className="icon"/>
+                    </div>
+                    <div className="remember-forgot">
+                        <label><input type="checkbox"/>Remember Me</label>
+                        <a href="#">Forgot password?</a>
+                    </div>
+                    <NavLink to="dashboard"><button className="login-btn" type="submit">Login</button></NavLink>
+                    <div className="register-link">
+                        <p>Don't have an account? <NavLink to="/signup">Register</NavLink></p>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,28 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser,FaLock, FaKey } from "react-icons/fa";
+import './SignUp.css'
 
 function SignUp() {
 
+    const [signupFormData, setSignupFormData] = useState({
+        username: '',
+        password: '',
+        confirmPassword: '',
+        successMsg: ''
+    })
+    
+    const [signupFormError, setSignupFormError] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    function validateFormInput(e){
+        e.preventDefault()
+        console.log("submitted")
+
+        let inputError = {
+            username: "",
+            password: "",
+            confirmPassword: ""
+        }
+
+        if (!signupFormData.email && !signupFormData.password) {
+            setSignupFormError({
+                ...inputError,
+                username: "Enter a valid username",
+                password: "Password should not be empty"
+            })
+            return
+        }
+    }
+
+    function handleFormChange(e){
+        const { name, value } = e.target
+        setSignupFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
     return (
         <div className="outside">
-        <div className="wrapper">
-            <form action="">
-                <h1>Sign Up</h1>
-                <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUser className="icon"/>
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Password" required/>
-                    <FaLock className="icon"/>
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Confirm Password" required/>
-                    <FaKey className="icon"/>
-                </div>
-                <button type="submit">Get Started!</button>
-            </form>
-        </div>
+            <div className="wrapper">
+                <form action="" onSubmit={validateFormInput}>
+                    <h1>Sign Up</h1>
+                    <div className="input-box">
+                        <input 
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            value={signupFormData.username}
+                            onChange={handleFormChange}
+                        />
+                        <FaUser className="icon"/>
+                        <p className="error-message">{signupFormError.username}</p>
+                    </div>
+                    <div className="input-box">
+                        <input 
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            value={signupFormData.password}
+                            onChange={handleFormChange}
+                        />
+                        <FaLock className="icon"/>
+                        <p className="error-message">{signupFormError.password}</p>
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            placeholder="Confirm Password"
+                            value={signupFormData.confirmPassword}
+                            onChange={handleFormChange}
+                        />
+                        <FaKey className="icon"/>
+                        <p className="error-message">{signupFormError.confirmPassword}</p>
+                        <p className="success-message">{signupFormData.successMsg}</p>
+                    </div>
+                    <button type="submit">Get Started!</button>
+                </form>
+            </div>
         </div>
 
     )
